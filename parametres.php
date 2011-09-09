0c4bd0dd072cde9a049addfe5418f57e5f2dee48
David Chavalarias <david.chavalarias@polytechnique.edu>
2011-09-09 03:23:20 +0200

    table scholars2terms et table des scholars

--- parametres.php
+++ parametres.php
@@ -5,7 +5,7 @@ list de parametres.
  */
 
 $dbname='community.db';
-$scholars_db ="scholars";
+$scholars_db ="raw_scholars";
 $fichier = "Scholars23May_2011.csv";
 //$fichier = "test2.csv";
 $drop_tables=true; // on efface les tables
