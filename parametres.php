7e7ece775ef0c63e345c1ab38ffd8914899d8067
David Chavalarias <david.chavalarias@polytechnique.edu>
2011-10-24 17:29:44 +0200

    saving changes

--- parametres.php
+++ parametres.php
@@ -5,8 +5,10 @@ list de parametres.
  */
 
 $dbname='community.db';
+$dbname='scholar_test_data.db';
 //$scholars_db ="raw_scholars";
 $fichier = "CSSscholars20Oct2011.csv";
+$fichier = "CSSscholars2Oct2011.csv";
 
 $min_num_friends=0;// nombre minimal de voisin que doit avoir un scholar pour être affiché
 //$fichier = "Scholars13Sept2011.csv";
@@ -18,9 +20,9 @@ $file_sep=',';
 //$scholar_filter=" where country='France' AND want_whoswho='Yes' AND css_member='Yes'";
 //$scholar_filter=" where css_member='Yes' AND want_whoswho='Yes'";
 //$scholar_filter=" where css_member='Yes' OR want_whoswho='Yes'";
-//$scholar_filter="where css_member='Yes'";
+$scholar_filter="where css_member='Yes'";
 //$scholar_filter="";
-$scholar_filter=" where country='France' AND want_whoswho='Yes'";
+//$scholar_filter=" where country='France' AND want_whoswho='Yes'";
 //$scholar_filter=" where country='France'";
 
 $compress='No';
